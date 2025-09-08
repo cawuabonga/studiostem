72c246e080cf14e2a5db10d15c158df2395af567
GABRIEL ALVA <juan.gabriel.alva1999@gmail.com>
2025-09-08 21:10:35 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,34 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'istjaq.edu.pe',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
