c9dddc612310908b165feb087f03165505c47007
GABRIEL ALVA <juan.gabriel.alva1999@gmail.com>
2025-09-14 06:13:18 +0000

    parece que hay un problema con el netxplugin

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,12 +1,10 @@
 
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {nextPlugin} from '@genkit-ai/next';
 
 export const ai = genkit({
   plugins: [
     googleAI(),
-    nextPlugin,
   ],
   model: 'googleai/gemini-2.0-flash',
 });
