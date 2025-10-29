f1d9d0dac91a3a08b63e2949581b79d14e62aefe
GABRIEL ALVA <juan.gabriel.alva1999@gmail.com>
2025-10-29 03:40:18 +0000

    trate de subir la imagen a la unidad didactica y aparece esto

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         hostname: 'istjaq.edu.pe',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
