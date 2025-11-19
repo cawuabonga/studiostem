4518ae021a4480260f462388da1e0eec86ad65d3
GABRIEL ALVA <juan.gabriel.alva1999@gmail.com>
2025-11-19 05:37:49 +0000

    aun hay errores, no permite subir las imagenes, en las dos secciones que

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -30,5 +30,3 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ error: 'Error al subir el archivo.', details: e.message, stack: e.stack }, { status: 500 });
   }
 }
-
-    
